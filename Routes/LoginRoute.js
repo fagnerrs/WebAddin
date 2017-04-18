a11d5a1ffd154f52ab2ac5b55ff10996a0d3f647
fagnerrs <fagnerrs@gmail.com>
2017-04-18 20:31:44 -0300

    Adding rest api and changing to https

---
+++ Routes/LoginRoute.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('getting');
+});
+
+router.post('/register', function (req, res) {
+    res.send('registering');
+});
+
+/* params
+   Content-Type: application/json 
+   body: { "Username":"user@fluxx.io", "Password":"" } */
+router.post('/login', function (req, res, next) {
+    
+    res.send('Username: ' + req.body.Username);
+});
+
+router.get('/logout', function (req, res) {
+    res.send('logout');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 // variables
 var http = require('http');
+var https = require('https');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var fs = require('fs');
 
 ///// CONFIGURATIONS ///////////////////////////
 var config = require('./Config/config.js');
+
+// declaring routes
+var loginRoute = require('./Routes/LoginRoute');
 
 // setting configuration
 var app = express();
 app.set('port', config.server_port);
 app.set('address', config.server_ip_address);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use(express.static('./'));
 
+app.use('/user', loginRoute);
+
+
 // start http server
-app.listen(app.get('port'), app.get('address'), function () {
-    console.log(" Server started at port: " + app.get('port'));
-    console.log(" Go to: https:// " + app.get('address') + ":" + app.get('port') + "/Views/Login/Login.html");
-});
+//app.listen(app.get('port'), app.get('address'), function () {
+    //console.log(" Server started at port: " + app.get('port'));
+    //console.log(" Go to: https:// " + app.get('address') + ":" + app.get('port') + "App/Views/Login/Login.html");
+//});
+
+https.createServer({
+    key: fs.readFileSync('./Cert/key.pem'),
+    cert: fs.readFileSync('./Cert/cert.pem')
+}, app).listen(443);
+
+
